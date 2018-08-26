77709f124756c24d8d9098d0264d5bf97b0ec934
Chase-Owens <chasejonathanowens@gmail.com>
2018-08-26 01:02:44 -0500

    Clean-up

--- src/components/bookshelf.jsx
+++ src/components/bookshelf.jsx
@@ -5,22 +5,21 @@ import Read from "./read";
 import { Link } from "react-router-dom";
 
 const Bookshelf = ({ moveShelf, books }) => {
-    return (
-      <main className="container">
-        <div className="list-books">
-          <h1 className="list-books-title">Your Digital Bookshelf</h1>
-          <div className="list-books-container">
-            <CurrentlyReading shelf={books} moveBook={moveShelf} />
-            <WantToRead shelf={books} moveBook={moveShelf} />
-            <Read shelf={books} moveBook={moveShelf} />
-          </div>
-          <div className="open-search">
-            <Link to="/search">Add a book</Link>
-          </div>
+  return (
+    <main className="container">
+      <div className="list-books">
+        <h1 className="list-books-title">Your Digital Bookshelf</h1>
+        <div className="list-books-container">
+          <CurrentlyReading shelf={books} moveBook={moveShelf} />
+          <WantToRead shelf={books} moveBook={moveShelf} />
+          <Read shelf={books} moveBook={moveShelf} />
         </div>
-      </main>
-    );
-
-}
+        <div className="open-search">
+          <Link to="/search">Add a book</Link>
+        </div>
+      </div>
+    </main>
+  );
+};
 
 export default Bookshelf;
--- src/components/read.jsx
+++ src/components/read.jsx
@@ -2,40 +2,39 @@ import React from "react";
 import Book from "./book";
 
 const Read = ({ shelf, moveBook }) => {
-    return (
-      <div className="bookshelf">
-        <h2 className="bookshelf-title container-fluid">Read</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            {shelf.filter(library => library.shelf === "read").map(
-              (book, i) =>
-                book.imageLinks ? (
-                  <li key={i}>
-                    <Book
-                      title={book.title}
-                      authors={book.authors}
-                      img={book.imageLinks.thumbnail}
-                      book={book}
-                      shelf={book.shelf}
-                      newShelf={moveBook}
-                    />
-                  </li>
-                ) : (
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title container-fluid">Read</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {shelf.filter(library => library.shelf === "read").map(
+            (book, i) =>
+              book.imageLinks ? (
+                <li key={i}>
                   <Book
                     title={book.title}
                     authors={book.authors}
-                    img={""}
+                    img={book.imageLinks.thumbnail}
                     book={book}
                     shelf={book.shelf}
                     newShelf={moveBook}
                   />
-                )
-            )}
-          </ol>
-        </div>
+                </li>
+              ) : (
+                <Book
+                  title={book.title}
+                  authors={book.authors}
+                  img={""}
+                  book={book}
+                  shelf={book.shelf}
+                  newShelf={moveBook}
+                />
+              )
+          )}
+        </ol>
       </div>
-    );
-
-}
+    </div>
+  );
+};
 
 export default Read;
