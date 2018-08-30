2e636d22a9edb6ef5be3f57db5289c3337638174
Chase-Owens <chasejonathanowens@gmail.com>
2018-08-30 07:09:48 -0500

    HashRouter

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter } from "react-router-dom";
+import { Router } from "react-router";
+import createHashHistory from "history/createHashHistory";
 
+const hashHistory = createHashHistory({ basename: process.env.PUBLIC_URL });
 ReactDOM.render(
-  <BrowserRouter>
+  <Router history={hashHistory}>
     <App />
-  </BrowserRouter>,
+  </Router>,
   document.getElementById("root")
 );
 registerServiceWorker();
